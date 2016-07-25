12986e446bd073ac3de557718a95fcc3944640c0
chuen <xuan60702@gmail.com>
2016-07-25 13:44:03 +0800

    finsh

--- perferct.py
+++ perferct.py
@@ -1,4 +1,2 @@
-def factors(n):
-    return [i for i in range (2, n/2 + 1) if n % i == 0]
 def perfect(n):
-    print  sum (factors(n)) + 1 == n
+    print  sum ([i for i in range (2, n/2 + 1) if n % i == 0]) + 1 == n
