0d8cb88ba55eda52954ec2865c53b9a5dc363732
chuen <xuan60702@gmail.com>
2016-07-25 15:48:33 +0800

    finsh

--- compare.py
+++ compare.py
@@ -1,15 +1,12 @@
-with open ('file.txt', 'r') as f:
-     num = f.read().split('\n')
-     num = map(int, num)
-
 def compare():
-     Max = num[0]
-     for i in range (len(num)):
-         if num[i] > Max:
-             Max = num[i]
-     print Max
-     num.remove(Max)
-
-for _ in range (3):
-     compare()
-
+    with open ('file.txt') as f:
+         num = map(int, f.read().split('\n'))
+    array = []
+    for _ in range (3):
+        Max = num[0]
+        for i in range (len(num)):
+            if num[i] > Max:
+                Max = num[i]
+        num.remove(Max)
+        array.append(Max)
+    return array
