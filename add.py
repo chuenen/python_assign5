45d7093da40c260d78bb827deff120256c67dfae
chuen <xuan60702@gmail.com>
2016-07-25 14:47:59 +0800

    finish

--- add.py
+++ add.py
@@ -4,6 +4,7 @@ def add ():
      array = []
      for s in range(len(num)):
          for w in range (len(num[s])):
-             value += ord(num[s][w])-64
+             value = ord(num[s][w]) - 64
+             value += values
          array.append(value * (s+1))
      return sum(array)
