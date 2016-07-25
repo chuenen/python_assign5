65fc659345430ecdf7f28171c06d76ae867967ef
chuen <xuan60702@gmail.com>
2016-07-25 09:36:25 +0800

    final

---
+++ compare.py
@@ -0,0 +1,15 @@
+with open ('file.txt', 'r') as f:
+     num = f.read().split('\n')
+     num = map(int, num)
+
+ef compare():
+     Max = num[0]
+     for i in range (len(num)):
+         if num[i] > Max:
+             Max = num[i]
+     print Max
+     num.remove(Max)
+
+for _ in range (3):
+     compare()
+
