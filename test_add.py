4e78ad2dde0dea4790582c50239d73dc2b223cb3
chuen <xuan60702@gmail.com>
2016-07-25 17:22:07 +0800

    test_add

--- test_add.py
+++ test_add.py
@@ -2,7 +2,9 @@ import unittest
 from add import add
 class test_add(unittest.TestCase):
     def test1(self):
-        self.assertEqual(add(),96)
+        self.assertEqual(add('"ABCD","DEF","C"'),61)
+        self.assertEqual(add('"CDEF","ABC","FIJK"'),150)
+        self.assertEqual(add('"AAA","BBB"'),15)
 
 if __name__ == '__main__':
-    unittest.main()        
+    unittest.main()
