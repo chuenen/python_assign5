537f3e0bbcdaa9fd0e90edefe70e89399bef968d
chuen <xuan60702@gmail.com>
2016-07-25 16:46:05 +0800

    finish

--- listcomprehension.py
+++ listcomprehension.py
@@ -1,4 +1,4 @@
-odd = sum(map(lambda odd : odd ** 2, [odd for odd in range (1, 11) if odd % 2 != 0]))
-even = sum(map(lambda even : even ** 2, [even for even in range (1, 11) if even % 2 == 0]))
-odd-even
-
+def sub():
+    odd = sum(map(lambda odd : odd ** 2, [odd for odd in range (1, 11) if odd % 2 != 0]))
+    even = sum(map(lambda even : even ** 2, [even for even in range (1, 11) if even % 2 == 0]))
+    return odd-even
