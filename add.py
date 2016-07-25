97e817852bb2515ed12ea9fcbe6b4e52d58bfdca
chuen <xuan60702@gmail.com>
2016-07-25 14:46:37 +0800

    finish

--- add.py
+++ add.py
@@ -1,13 +1,9 @@
-with open('abc.txt') as f:
-     num = f.read().replace('"','').split(',')
-     num = sorted(num)
-
 def add ():
+     with open('abc.txt') as f:
+         num = sorted(f.read().replace('"','').split(','))
      array = []
-     n = 0
      for s in range(len(num)):
          for w in range (len(num[s])):
-             value = ord(num[s][w])-64
-             value += value
+             value += ord(num[s][w])-64
          array.append(value * (s+1))
-     return sum(array) 
+     return sum(array)
