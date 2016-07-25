ef0aa9399ddb0afd53e5bba96a4713eea935be4a
chuen <xuan60702@gmail.com>
2016-07-25 11:26:39 +0800

    finish

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,5 @@
 def fib():
-    f1, f2 = 0, 1
-    while True:
-        yield f1
-        f1, f2 = f1, f1+f2
-
-# 命名使解釋更容易
-# 符號間格
-# while True replace while 1
+      f1, f2 = 0, 1
+      while True:
+          yield f1
+          f1, f2 = f2, f1 + f2
