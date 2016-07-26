4a4e2c8f69d548fabc78ba13e0f417f854c54c80
chuen <xuan60702@gmail.com>
2016-07-26 10:09:19 +0800

    test_cmp

--- test_cmp.py
+++ test_cmp.py
@@ -1,8 +1,10 @@
 import unittest
 from compare import compare
 class test_cmp(unittest.TestCase):
-    def test1(self):        
-        self.assertEqual(compare(), [9239, 8723, 6421])
+    def test1(self):
+        self.assertEqual(compare('2134,3412,6421,8723,9239,1234,2345'), [9239, 8723, 6421])
+        self.assertEqual(compare('12345,6666666,99776,567,88888,456789,34'), [6666666, 456789, 99776])
+        self.assertEqual(compare('1,3,5,7,9,10,36,8,33,15'), [36, 33, 15])
 
 if __name__ == '__main__':
     unittest.main()
