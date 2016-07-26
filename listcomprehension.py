7b3068dacd67907f19043ed227ba4347ab2c9075
chuen <xuan60702@gmail.com>
2016-07-26 09:55:09 +0800

    finish

--- listcomprehension.py
+++ listcomprehension.py
@@ -1,4 +1,4 @@
-def sub():
-    odd = sum(map(lambda odd : odd ** 2, [odd for odd in range (1, 11) if odd % 2 != 0]))
-    even = sum(map(lambda even : even ** 2, [even for even in range (1, 11) if even % 2 == 0]))
+def sub(a, b):
+    odd = sum(map(lambda odd : odd ** 2, [odd for odd in range (a, b+1) if odd % 2 != 0]))
+    even = sum(map(lambda even : even ** 2, [even for even in range (a, b+1) if even % 2 == 0]))
     return odd-even
--- test_listcom.py
+++ test_listcom.py
@@ -2,8 +2,8 @@ import unittest
 from listcomprehension import sub
 class test_listcom(unittest.TestCase):
     def test1(self):
-        self.assertEqual(sub(), -55)
+        self.assertEqual(sub(1, 10), -55)
+        self.assertEqual(sub(1, 100), -5050)
 
 if __name__ == '__main__':
     unittest.main()
-    
