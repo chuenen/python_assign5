b4f0cf820fb7f2d9d7110b17dd34432c21c81f34
chuen <xuan60702@gmail.com>
2016-09-23 14:35:06 +0800

    Let the test run fail

--- test_fib.py
+++ test_fib.py
@@ -6,7 +6,7 @@ class test_fib(unittest.TestCase):
         self.assertEqual(f.next(), 1)
         self.assertEqual(f.next(), 2)
         self.assertEqual(f.next(), 3)
-        self.assertEqual(f.next(), 5)
+        self.assertEqual(f.next(), 4)
         self.assertEqual(f.next(), 8)
     def test2(self):
         f = fib()
